8aed4609191c990d7acc361d65abe50786d784d8
Serch <serch7ev@gmail.com>
2026-09-23 19:50:25 -0600

    Add files via upload
    2do commit, lab2_calculadora basica

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-#Fizzbuzz
+print("Bienvenido a la calculadora");
+num1 = float(input("Ingrese el primer número: "));
+num2 = float(input("Ingrese el segundo número: "));
+result = num1 + num2
+print(f"El resultado de la suma es: {result}")
+print("\n\nMas operaciones disponibles: \n 1. Suma otro numero mas \n 2. Resta \n 3. Multiplicación \n 4. División");
 
-for num in range(1, 1001):
-    if num % 3 == 0 and num % 5 == 0:
-        print(num, "FizzBuzz")
-    elif num % 3 == 0:
-        print(num, "Fizz")
-    elif num % 5 == 0:
-        print(num, "Buzz")
-    else:
-        print(num)
-print("\n Fin del programa. \n")
+case = input("Ingrese el numero correspondiente a la operación a realizar: ");
+match case:
+    case "1":
+        num3 = float(input("Ingrese el tercer número: "));
+        result = num1 + num2 + num3
+        print(f"El resultado de la suma  de tres numeros es: {result}")
+    case "2":
+        result = num1 - num2
+        print(f"El resultado de la resta es: {result}")
+    case "3":
+        result = num1 * num2
+        print(f"El resultado de la multiplicación es: {result}")
+    case "4":
+        if num2 != 0:
+            result = num1 / num2
+            print(f"El resultado de la división es: {result}")
+        else:
+            print("Error: No se puede dividir entre cero.")
